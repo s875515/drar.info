b3d636f9ba04d6a289a703d46ec4bb9e78c2526b
Bobo Git <s875515@gmail.com>
2016-04-13 09:18:17 +0800

    add text-overflow

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import {hashHistory, Router, Route, IndexRoute} from 'react-router';
 import Main from '../containers/Main';
 import Index from '../components/Index';
-import Page1 from '../components/Page1';
-import Page2 from '../components/Page2';
 
 export default (
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Index}/>
-      <Route path="page1" component={Page1} />
-      <Route path="page2" component={Page2} />
     </Route>
   </Router>
 );
